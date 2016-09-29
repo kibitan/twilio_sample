16e6d40f94bc4d6e4054bae8355f228136c20291
kibitan <uzukifirst@gmail.com>
2016-09-29 17:35:03 +0900

    add TOKEN retrive

--- twilio.rb
+++ twilio.rb
@@ -1,5 +1,10 @@
 require 'sinatra'
 
+TWILIO_NUMBER     = ENV['TWILIO_NUMBER']
+TWILIO_SID        = ENV['TWILIO_SID']
+TWILIO_AUTH_TOKEN = ENV['TWILIO_AUTH_TOKEN']
+
+
 get '/' do
   'Hello world!'
 end
