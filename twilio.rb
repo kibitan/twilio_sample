466e93368e156adde80d235cae744c7dccd06094
kibitan <uzukifirst@gmail.com>
2016-10-13 20:17:58 +0900

    add debuging gems

--- twilio.rb
+++ twilio.rb
@@ -1,4 +1,13 @@
 require 'sinatra'
+require 'pry'
+require 'better_errors'
+require 'slim'
+require 'twilio-ruby'
+
+configure :development do
+  use BetterErrors::Middleware
+  BetterErrors.application_root = __dir__
+end
 
 TWILIO_NUMBER     = ENV['TWILIO_NUMBER']
 TWILIO_SID        = ENV['TWILIO_SID']
