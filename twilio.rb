eb13864a6bc601e4d42254908fd7b4a56c77b545
kibitan <uzukifirst@gmail.com>
2016-10-13 20:27:25 +0900

    basic calling method

--- twilio.rb
+++ twilio.rb
@@ -13,7 +13,30 @@ TWILIO_NUMBER     = ENV['TWILIO_NUMBER']
 TWILIO_SID        = ENV['TWILIO_SID']
 TWILIO_AUTH_TOKEN = ENV['TWILIO_AUTH_TOKEN']
 
+Twilio.configure do |config|
+  config.account_sid = TWILIO_SID
+  config.auth_token  = TWILIO_AUTH_TOKEN
+end
 
 get '/' do
-  'Hello world!'
+  slim :index
+end
+
+post '/call' do
+  client = Twilio::REST::Client.new
+  calling = client.account.calls.create(
+    from: TWILIO_NUMBER,
+    to:  params["to_number"],
+    url: (host_name + '/callback').to_s,
+    method: "GET" # default は POST
+  )
+end
+
+get '/callback' do
+end
+
+private
+
+def host_name
+  URI.parse("#{request.env['rack.url_scheme']}://#{request.env["HTTP_HOST"]}")
 end
